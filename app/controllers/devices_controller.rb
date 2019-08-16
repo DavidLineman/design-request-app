1915e96325358f741cb377913906d4c67b29ad44
DavidLineman <dline215@gmail.com>
2019-08-16 15:43:03 -0400

    added create method

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -5,6 +5,17 @@ class DevicesController < ApplicationController
   end
 
   def new
-    @devices = Device.new
+    @device = Device.new
+  end
+
+  def create
+    Device.create!(device_params)
+    redirect_to root_path
+  end
+
+  private 
+
+  def device_params
+    params.require(:device).permit(:manufacturer, :model, :description)
   end
 end
